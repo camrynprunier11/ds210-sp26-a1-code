d75690b9f69bb215c2a39f5f1da58ced9195435e
Camryn Prunier <camryn11@bu.edu>
2026-02-13 00:57:38 -0500

    solution for part2

--- homework_3_guessing_game/src/part2.rs
+++ homework_3_guessing_game/src/part2.rs
@@ -6,7 +6,16 @@ pub struct Part2 {}
 // Terrible strategy: ask if the number is min, otherwise return max.
 impl Strategy for Part2 {
     fn guess_the_number(player: &mut Player, min: u32, max: u32) -> u32 {
-        // YOUR SOLUTION GOES HERE.
-        todo!("No solution provided yet")
+        let guess = (min + max) / 2;
+        let temp = player.ask_to_compare(guess);
+        if temp == 0 {
+            return guess;
+        } else if temp == 1 {
+            return Self::guess_the_number(player, guess, max); //Google searched how to do recursion with Rust
+        } else if temp == -1 {
+            return Self::guess_the_number(player, min, guess);
+        } else {
+            return 0;
+        }
     }
 }
